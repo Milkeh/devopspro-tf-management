3410f55f7335a8b01d857cbec76a91f993c2245f
Harley McKenzie <harley@akoova.com>
2022-09-17 15:51:03 +0100

    Connected GitHub VCS to codepipeline-website Workspace

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ resource "tfe_organization" "devopspro" {
 
 resource "tfe_workspace" "codepipeline-website" {
   name         = "codepipeline-website"
+  description = "Static website fully managed through AWS CodePipeline"
   organization = tfe_organization.devopspro.name
+  vcs_repo {
+    identifier  = "Milkeh/devopspro-tf-codepipeline-website"
+    oauth_token_id = var.oauth_token_id
+  }
   tag_names    = ["codepipeline"]
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "hostname" {
   default = "app.terraform.io"
   description = "Hostname of the Terraform Cloud/Enterprise host to connect to"
 }
+
+variable "oauth_token_id" {
+  description = "The VCS Connection (OAuth Connection + Token) to use"
+}
