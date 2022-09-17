5d18c6b5684cca1e24afdea19ea8dea6f1b9ad9b
Harley McKenzie <harley@akoova.com>
2022-09-17 15:21:56 +0100

    Recreate devopspro organization and add codepipeline-website workspace

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "tfe_organization" "devopspro-tf" {
-  name  = "devopspro-tf"
+resource "tfe_organization" "devopspro" {
+  name  = "devopspro"
   email = "mckenzie.harley@gmail.com"
+}
+
+resource "tfe_workspace" "codepipeline-website" {
+  name         = "codepipeline-website"
+  organization = tfe_organization.devopspro.name
+  tag_names    = ["codepipeline"]
 }
